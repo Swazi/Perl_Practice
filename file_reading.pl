92e197993fe014f9ce37c59b4aafbdf4b3810fda
Swazi <alexs.123123@gmail.com>
2012-11-27 13:29:34 -0500

    changed a few things

--- file_reading.pl
+++ file_reading.pl
@@ -1,10 +1,12 @@
 use strict;
 use warnings;
 use 5.010;
+use autodie;
 
 my $filename = 'data.txt';
 # If it is not necessary to die if the file is not found
 # use an if(open) because open will evaluate to true or false
+# you can also use autodie
 open(my $fh, '<', $filename) or die "Could not open file '$filename' $!";
 
 while (my $row = <$fh>) {
