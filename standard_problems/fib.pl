04367886a55a96cc792104f6ba6a20f6d6bcb707
Swazi <alexs.123123@gmail.com>
2012-11-27 20:20:19 -0500

    updated fib

--- standard_problems/fib.pl
+++ standard_problems/fib.pl
@@ -1,21 +1,18 @@
 #!/usr/bin/perl
-
-# Functions in Perl are called subroutines. Like bash, you don't define the 
-# parameters in the function declaration. We'll see how to access the parameters
-# when we look at the Fibonacci subroutine below.
+use 5.010;
+use strict;
+use warnings;
 
 sub main {
 
-# Again, the ubiquitous printf function. Apparently a staple of programming
-printf "\nHow many numbers of the sequence would you like?\n";
 
-# Once again, we don't need to declare variables before using them.
-# Perl's scalar variables, prefaced with $, can be either strings or numbers
+say "\nHow many numbers of the sequence would you like?\n";
+
 # We use <STDIN> to get the data from stdin here
 
-$n = <STDIN>;
+my $n = <STDIN>;
 
-# As in PHP, we need to remove the newline at the end
+# Remove the newline at the end
 
 chomp $n;
 
@@ -24,17 +21,10 @@ chomp $n;
 exit 0;
 }
 
-# Except for the first line declaring the subroutine, and the different way that
-# parameters passed to the subroutine are passed, this is identical to the PHP version
-
+# The fibonacci sub
 sub fibonacci {
   $a = 0;
   $b = 1;
-
-  # So now we see the parameter being used here. For clarity, I have written the for
-  # loop using $n like the other examples. To set $n using the first parameter passed
-  # to the subroutine, I access the scalar variable $_[0], which is the first element
-  # of the parameter array @_
 
   $n = $_[0];
 
