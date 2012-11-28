1506a5b5147c0980a258ed849044dc60aa877286
Swazi <alexs.123123@gmail.com>
2012-11-27 19:57:20 -0500

    binary search

---
+++ standard_problems/binary_search.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use 5.010;
+use strict;
+use warnings;
+
+
+
+# A binary search algorithm for arrays of strings 
+# Returns the index of the target or the position the
+# target could be inserted into the array keeping the 
+# array sorted
+
+sub bin_search_string {
+    my ($array, $target) = @_;
+
+    my ( $min, $max ) = ( 0, scalar(@$array) );
+
+    while ( $min < $max ) {
+	use integer;
+	my $current = ($min+$max)/2;
+	if ($array->[$current] lt $target) {
+	    $min = $current + 1;
+	} else {
+	    $max = $current;
+	}
+    }
+
+    return $min;
+}
+
+my $letter = "D";
+
+my @alphabet = qw(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z);
+
+print "Index of $letter in the alphabet is: ", bin_search_string( \@alphabet, $letter ), "\n";
